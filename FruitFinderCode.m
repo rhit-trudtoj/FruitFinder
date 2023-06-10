bbe2053958a337759182a74b44b4d00b49970e69
rhit-trudtoj <78326617+rhit-trudtoj@users.noreply.github.com>
2023-06-10 00:08:11 -0400

    100% working fruit finder OwenT

--- FruitFinderCode.m
+++ FruitFinderCode.m
@@ -30,7 +30,7 @@ for num = 1:numFruit
     
     %apple mask
     mask = zeros(size(img_hsv(:,:,1)));
-    redIdx = find((h <= 0.03 | h >= 0.9) & (s >= 0.4) & (v >= 0.02));
+    redIdx = find((h <= 0.05 | h >= 0.9) & (s >= 0.5) & (v >= 0.045));
     mask(redIdx) = 1;
     reg_mask(:,:,1) = mask; 
     
@@ -44,35 +44,95 @@ for num = 1:numFruit
     
     %lot of noise from background for some reason
     mask = zeros(size(img_hsv(:,:,1)));
-    yellowIdx = find((h >= 0.1 & h <= 0.25) & (s >= 0.35) & (v >= 0.55));
+    yellowIdx = find((h >= 0.1 & h <= 0.25) & (s >= 0.5) & (v >= 0.35));
     mask(yellowIdx) = 1; 
     reg_mask(:,:,3) = mask;
     
     %then want to process each mask and do morphological operations and
+    %then do 
     %component analysis 
     
-%     for i = 1:3
+    for i = 1:3
 %         
 %         %morphological operations
-%         se = strel('square', 3);
-%         
-%         reg_mask(:,:,i) = imclose(reg_mask(:,:,i), se); 
-%         reg_mask(:,:,i) = imopen(reg_mask(:,:,i), se);
-%     
-%     end
+          %need to get rid of pixels before operations
+          
+          temp_mask = reg_mask(:,:,i); 
+          
+          %get average cluster size and remove those that are <1/3 of the
+          %average
+          components = bwconncomp(temp_mask); 
+          pixels = cellfun(@numel, components.PixelIdxList); 
+          avg = mean(pixels);
+          remove = find(pixels < avg/3);
+          for n = 1:length(remove)
+              
+              temp_mask(components.PixelIdxList{remove(n)}) = 0;
+       
+          end
+          
+          reg_mask(:,:,i) = temp_mask;
+          
+          %get average cluster size and remove those that are <50
+          components = bwconncomp(temp_mask); 
+          pixels = cellfun(@numel, components.PixelIdxList); 
+          remove = find(pixels < 100);
+          for n = 1:length(remove)
+              temp_mask(components.PixelIdxList{remove(n)}) = 0; 
+          end
+          
+          reg_mask(:,:,i) = temp_mask;
+          
+          
+          
+         se = strel('square', 3);
+          
+         reg_mask(:,:,i) = imclose(reg_mask(:,:,i), se); 
+         reg_mask(:,:,i) = imopen(reg_mask(:,:,i), se); 
+         
+         
+         components = bwconncomp(temp_mask); 
+         pixels = cellfun(@numel, components.PixelIdxList); 
+         avg = mean(pixels);
+         remove = find(pixels < avg/2);
+         for n = 1:length(remove)
+             temp_mask(components.PixelIdxList{remove(n)}) = 0; 
+         end
+         
+         reg_mask(:,:,i) = temp_mask;
+          
+         
+    end
+
+    %do connected component analysis
+    
+    figure(num);
+    imshowpair(reg_mask, img,'montage'); 
+    title('Identifying Mask and the Original Image');
+    
+    apples = bwlabel(reg_mask(:,:,1));
+    data_apples = regionprops('table', apples, 'Centroid', 'Area'); 
+    
+    oranges = bwlabel(reg_mask(:,:,2));
+    data_oranges = regionprops('table', oranges, 'Centroid', 'Area');
+    
+    bannanas = bwlabel(reg_mask(:,:,3)); 
+    data_bannanas = regionprops('table', bannanas, 'Centroid', 'Area');
+    
+    
     
     %save masks
-    if ~isempty(target)
-        fruit = target{1};
-    
-        filename = ['fruitFinderMask_', fruit, '.png'];
-    
-        imwrite(reg_mask, filename); 
-        
-    else
-        disp('String not found.');
-    
-    end
+%     if ~isempty(target)
+%         fruit = target{1};
+%     
+%         filename = ['fruitFinderMask_', fruit, '.png'];
+%     
+%         imwrite(reg_mask, filename); 
+%         
+%     else
+%         disp('String not found.');
+%     
+%     end
     
     
 end 
